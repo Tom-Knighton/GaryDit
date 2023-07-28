5db3aacd826185e6d5322560ea6b31004467330c
Tom K <tomknighton@icloud.com>
2023-07-28 14:10:13 +0100

    GDIT-22: Resolve media control timeout when interacting with controls

--- GaryDit/Views/Media/MediaGalleryView.swift
+++ GaryDit/Views/Media/MediaGalleryView.swift
@@ -83,6 +83,7 @@ struct MediaGalleryView: View {
                 }
                 .tabViewStyle(.page(indexDisplayMode: self.viewModel.displayControls ? .automatic : .never))
                 .indexViewStyle(.page(backgroundDisplayMode: self.viewModel.displayControls ? .always : .interactive))
+                .simultaneousGesture(exclusiveTapGestures())
             }
             
             VStack(spacing: 0) {
@@ -107,7 +108,7 @@ struct MediaGalleryView: View {
                         .padding(.horizontal, 16)
                 }
                 if let binding = Binding<VideoPlayerViewModel>($currentMediaViewModel) {
-                    MediaControlsView(mediaViewModel: binding, previewImage: $viewModel.scrubThumbnail)
+                    MediaControlsView(mediaViewModel: binding, galleryViewModel: $viewModel, previewImage: $viewModel.scrubThumbnail)
                 }
                 Spacer().frame(height: 40)
             }
@@ -117,7 +118,6 @@ struct MediaGalleryView: View {
         }
         .opacity(viewModel.entireViewOpacity)
         .simultaneousGesture(dragAwayGesture($draggingOffset))
-        .simultaneousGesture(exclusiveTapGestures())
         .onChange(of: viewModel.selectedTabUrl, initial: true) {
             let vm = postModel.videoViewModels.first(where: { $0.media.url == viewModel.selectedTabUrl })
             self.currentMediaViewModel = vm
